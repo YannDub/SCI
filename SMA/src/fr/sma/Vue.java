84ae0631e848e6cf808125bbb99e62b0254dd60a
Dubois Yann <dubois@b02p7.fil.univ-lille1.fr>
2017-09-19 16:23:02 +0200

    Add Vue

---
+++ SMA/src/fr/sma/Vue.java
@@ -0,0 +1,13 @@
+package fr.sma;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class Vue implements Observer {
+
+	@Override
+	public void update(Observable o, Object arg) {
+		
+	}
+
+}
